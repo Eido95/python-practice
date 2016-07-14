951306c43b30f0b66e8949ac9f1dc0ba8388bd75
Eido Askayo <eido.askayo@gmail.com>
2016-07-14 11:17:46 +0300

    first server implementation

--- python3/authorization-system/server/core.py
+++ python3/authorization-system/server/core.py
@@ -1,4 +1,15 @@
 #!/usr/bin/python
+# https://wiki.python.org/moin/BaseHttpServer
+# https://www.djangoproject.com/
+# http://www.linuxjournal.com/content/tech-tip-really-simple-http-server-python
+# https://wiki.python.org/moin/WebProgramming
+# https://hg.python.org/cpython/file/2.7/Lib/BaseHTTPServer.py
 
+import time
+import BaseHTTPServer
 
+HOST_NAME = "localhost"
+PORT = "8082" #unpreseved unofficial port
+
+# continue from https://docs.python.org/3.5/tutorial/classes.html
 print("I'm a python server!")
