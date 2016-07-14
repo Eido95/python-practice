9ab63918da698248efc797d01923fdc272f6edf2
Eido Askayo <eido.askayo@gmail.com>
2016-07-14 16:25:15 +0300

    continue to implement http request handler class

--- python3/authorization-system/server/core.py
+++ python3/authorization-system/server/core.py
@@ -6,13 +6,46 @@
 # https://hg.python.org/cpython/file/2.7/Lib/BaseHTTPServer.py
 # https://docs.python.org/3/library/http.server.html
 # https://docs.python.org/3/tutorial/modules.html
-# The BaseHTTPServer module has been merged into http.server in Python 3.
+# https://daanlenaerts.com/blog/2015/06/03/create-a-simple-http-server-with-python-3/
+## The BaseHTTPServer module has been merged into http.server in Python 3.
+# http_URL = "http:" "//" host [ ":" port ] [ abs_path [ "?" query ]]
+
+__version__ = "0.1"
 
 import time
-import http.server
+from http import server
 
 HOST_NAME = "localhost"
 PORT_NUMBER = "8082" #unpreseved unofficial port
+
+class EidoHTTPRequestHandler(server.BaseHTTPRequestHandler):
+    """This class is used to handle the HTTP requests that arrive at the server."""
+    def __init__(self):
+        """Called after the instance has been created (by __new__()), but before it is returned to the caller."""
+        super().__init__ # This is useful for accessing inherited methods that have been overridden in a class.
+        self.server_version = "EidoHTTP/" + __version__
+        self.protocol_version = "HTTP/1.1" #https://tools.ietf.org/html/rfc2616
+
+    # The handler will parse the request and the headers, then call a method specific to the request type. The method name is constructed from the request. For example, for the request method POST, the do_POST() method will be called with no arguments.
+    def do_GET(self):
+        """
+        Serve a GET request - client requests data from the server.
+        The GET method means retrieve whatever information (in the form of an entity) is identified by the Request-URI (identifies the resource upon which to apply the request).
+        """
+        self.send_response(200) # Status code OK - Successful class
+        # An entity corresponding to the requested resource should be sent in the response;
+        # Continue with the following tabs:
+        # https://tools.ietf.org/html/rfc2616
+        # http://www.w3schools.com/tags/ref_httpmethods.asp
+        # https://daanlenaerts.com/blog/2015/06/03/create-a-simple-http-server-with-python-3/
+        # https://www.python.org/dev/peps/pep-0008/
+        # https://docs.python.org/3.5/
+        # https://docs.python.org/3.5/tutorial/
+        # https://docs.python.org/3/library/http.server.html
+
+
+    def do_POST(self):
+        # client submits data to to the server to be proccessed.
 
 # class MyHandler(BaseHTTPServer.BaseHTTPRequestHandler):
 #     def do_head(s):
